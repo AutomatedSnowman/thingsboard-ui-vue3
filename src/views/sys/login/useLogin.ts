04c6ee6ec6967ef8d7b16ec59784c51c6107aedf
AutomatedSnowman <157937054+AutomatedSnowman@users.noreply.github.com>
2025-07-29 19:51:42 -0400

    Update useLogin.ts

--- src/views/sys/login/useLogin.ts
+++ src/views/sys/login/useLogin.ts
@@ -5,9 +5,9 @@ import { useI18n } from '/@/hooks/web/useI18n';
 export enum LoginStateEnum {
   LOGIN,
   REGISTER,
-  RESET_PASSWORD,
-  MOBILE,
-  QR_CODE,
+  RESET_PASSWORD
+  //MOBILE,
+  //QR_CODE,
 }
 
 const currentState = ref(LoginStateEnum.LOGIN);
